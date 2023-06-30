9d1a644310f79ce50a297c2346601ec91a8f55f0
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-06-29 20:20:07 -0600

    Intento de Conexiopn Firebase

--- AppUTH.Android/MainActivity.cs
+++ AppUTH.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Firebase;
 
 namespace AppUTH.Droid
 {
@@ -17,6 +18,7 @@ namespace AppUTH.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            FirebaseApp.InitializeApp(this);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ AppUTH/Views/PageRegistro.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+
+
+namespace AppUTH.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageRegistro : ContentPage
+	{
+		public PageRegistro ()
+		{
+			InitializeComponent ();
+		}
+
+        private  void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
